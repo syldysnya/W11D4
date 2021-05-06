70964a9169c7e6845791aab081a4b5789b5fc0f3
syldysnya <syldysnya@gmail.com>
2021-05-06 16:12:45 -0400

    Finish phase 1

--- BenchBnB/app/controllers/application_controller.rb
+++ BenchBnB/app/controllers/application_controller.rb
@@ -1,2 +1,27 @@
 class ApplicationController < ActionController::Base
+
+    helper_method :logged_in?, :current_user
+
+    def current_user
+        @current_user ||= User.find_by(session_token: session[:session_token])
+        return @current_user if @current_user
+    end
+
+    # def ensure_logged_in
+    #     redirect_to new_session_url
+    # end
+
+    def login!(user)
+        session[:session_token] = user.session_token
+    end
+
+    def logout!
+        current_user.reset_session_token!
+        session[:session_token] = nil
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
 end
